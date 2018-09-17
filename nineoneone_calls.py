3bc408d31f9005903a9c9559060eff9b27229b0a
Rodolfo <rodolfo.ern.marquez@gmail.com>
2018-09-16 22:23:57 -0400

    Answer basic questions

--- nineoneone_calls.py
+++ nineoneone_calls.py
@@ -3,3 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('911.csv')
+
+#BASIC QUESTIONS
+#TOP 5 ZIP
+df['zip'].value_counts().head()
+
+#TOP 5 TOWNSHIPS
+df['twp'].value_counts().head()
+
+#Unique Title Codes
+df['title'].nunique()

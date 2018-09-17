6c23aea55414e567c73ebbbc0eca0159c15e0bbc
Rodolfo <rodolfo.ern.marquez@gmail.com>
2018-09-17 12:49:30 -0400

    Create countplot of emergency reasons

--- nineoneone_calls.py
+++ nineoneone_calls.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.read_csv('911.csv')
 
@@ -13,3 +14,11 @@ df['twp'].value_counts().head()
 
 #Unique Title Codes
 df['title'].nunique()
+
+#CREATING NEW FEATURES
+#Get Reason - w/o details
+df['Reason'] = df['title'].str.split(':').apply(lambda x: x[0])
+df['Reason'].value_counts()
+
+sns.countplot(x='Reason',data=df)
+plt.show()
